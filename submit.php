1f8fe500ffa282163d66a29dd11d506b6a5db823
Gooper4876 <tymek.kowalewski2008@gmail.com>
2024-03-14 14:23:49 +0100

    bleh

---
+++ submit.php
@@ -0,0 +1,10 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "platforma_kulinarna_1K";
+
+if(conn->connect_error) {
+    die("bruh". $conn->connect_error);
+}
+?>
