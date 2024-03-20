7232bdcff622feda682e9b1e7c1e518bf12bdc5f
Gooper4876 <tymek.kowalewski2008@gmail.com>
2024-03-20 12:24:38 +0100

    yes

--- submit.php
+++ submit.php
@@ -7,4 +7,16 @@ $dbname = "platforma_kulinarna_1K";
 if(conn->connect_error) {
     die("bruh". $conn->connect_error);
 }
+$title = $_POST["title"];
+$description = $_POST["description"];
+
+$sql = "INSERT INTO przepisy(tytul) VALUES ('$title)";
+
+if (conn()->query($sql) === TRUE) {
+    echo "Yippe you did it";
+} else {
+    echo "bruh";
+}
+
+$conn->close();
 ?>
