7301af2b333ad2629d99e4c3658aaf133a04d233
Gooper4876 <tymek.kowalewski2008@gmail.com>
2024-04-24 11:12:35 +0200

    bro fr

--- mainpage.php
+++ mainpage.php
@@ -72,7 +72,7 @@
             <img src="Temmie.jpeg">
         </a>
     </p>
-    [why they have ourple bars]
+    [why they have blu bars]
     </p>
     <p>
         <recipesearch>
@@ -89,32 +89,11 @@
     <p>
         also how to change style for search box and buttons
     </p>
-    <?php
-    $servername = "localhost"; //nazwa servera
-    $username = "root"; //nazwa użytkownika bazy danych
-    $password = ""; //hasło do bazy
-    $dbname = "tem"; //nazwa bazy danych
     
-    $conn = new mysqli($servername, $username, $password, $dbname);
-
-    if ($conn->connect_error) {
-        die ("Connection failed, idiot" . $conn->connect_error);
-    } else {
-        echo "Connection works";
-    }
-
-    $sql = "SELECT * FROM przepisy";
-    $result = $conn->query($sql);
-
-    echo "<ol>";
-    while ($row = $result->fetch_assoc()) {
-        echo "<li>" . $row["title"] . "</li>";
-    }
-
-    echo "</ol>";
-
-    $conn->close();
-    ?>
 </body>
+<?php
 
+include "display_produkt.php";
+
+?>
 </html>
---
+++ recipes.php
@@ -0,0 +1,26 @@
+<?php
+    $servername = "localhost"; //nazwa servera
+    $username = "root"; //nazwa użytkownika bazy danych
+    $password = ""; //hasło do bazy
+    $dbname = "tem"; //nazwa bazy danych
+    
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    if ($conn->connect_error) {
+        die ("Connection failed, idiot" . $conn->connect_error);
+    } else {
+        echo "Connection works";
+    }
+
+    $sql = "SELECT * FROM przepisy";
+    $result = $conn->query($sql);
+
+    echo "<ol>";
+    while ($row = $result->fetch_assoc()) {
+        echo "<li>" . $row["title"] . "</li>";
+    }
+
+    echo "</ol>";
+
+    $conn->close();
+?>
