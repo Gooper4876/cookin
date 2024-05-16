7278126967bc472733a322f7776c662e89d55518
Gooper4876 <tymek.kowalewski2008@gmail.com>
2024-05-16 13:43:31 +0200

    reeh

--- mainpage.php
+++ mainpage.php
@@ -90,30 +90,7 @@
         also how to change style for search box and buttons
     </p>
     <?php
-    $servername = "localhost"; //nazwa servera
-    $username = "root"; //nazwa użytkownika bazy danych
-    $password = ""; //hasło do bazy
-    $dbname = "tem"; //nazwa bazy danych
-    
-    $conn = new mysqli($servername, $username, $password, $dbname);
-
-    if ($conn->connect_error) {
-        die ("Connection failed, idiot" . $conn->connect_error);
-    } else {
-        echo "Connection works";
-    }
-
-    $sql = "SELECT * FROM przepisy";
-    $result = $conn->query($sql);
-
-    echo "<ol>";
-    while ($row = $result->fetch_assoc()) {
-        echo "<li>" . $row["title"] . "</li>";
-    }
-
-    echo "</ol>";
-
-    $conn->close();
+    include "recipes.php";
     ?>
 </body>
 
